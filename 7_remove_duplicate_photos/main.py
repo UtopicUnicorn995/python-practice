f213106568dc3b2a5fffc73882e2647169550d7e
utopicUnicorn <christianbarrios995@gmail.com>
2024-05-30 21:11:41 +0800

    python-practice: added codes for converting password

--- 7_remove_duplicate_photos/main.py
+++ 7_remove_duplicate_photos/main.py
@@ -3,10 +3,13 @@ import glob
 from PIL import Image
 import imagehash
 
-images = []
-for image in glob.iglob(f"{os.getcwd()}/assets/*"):
-    if str(imagehash.average_hash(Image.open(image))) in images:
-        os.remove(image)
-    else:
-        images.append(str(imagehash.average_hash(Image.open(image))))
-    
+def main():
+    images = []
+    for image in glob.iglob(f"{os.getcwd()}/assets/*"):
+        if str(imagehash.average_hash(Image.open(image))) in images:
+            os.remove(image)
+        else:
+            images.append(str(imagehash.average_hash(Image.open(image))))
+
+if __name__ == "__main__":
+    main()
--- 8_convert_password/main.py
+++ 8_convert_password/main.py
@@ -1,8 +1,25 @@
 import random
 
 class Passwords:
-    def __init__(self):
-        pass
+    def __init__(self, length):
+        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+        self.generated_password = ''.join(random.choice(characters) for i in range(length))
+        
     
     def check_password_strength(self):
-        pass
+         # Define criteria for a strong password
+        criteria = {
+            'length': len(self.generated_password) >= 8,
+            'lowercase': any(char.islower() for char in self.generated_password),
+            'uppercase': any(char.isupper() for char in self.generated_password),
+            'digit': any(char.isdigit() for char in self.generated_password),
+            'special': any(char in "!@#$%^&*()" for char in self.generated_password)
+        }
+        strength = sum(criteria.values())
+        
+        return strength
+    
+password = Passwords(12)
+print("Generated password:", password.generated_password)
+
+print("Password strength:", password.check_password_strength())
