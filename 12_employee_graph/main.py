5fef44c1ee5d99d3be3d9b9c089f15d95961a41d
utopicUnicorn <christianbarrios995@gmail.com>
2024-06-13 21:52:58 +0800

    python-practice: added item 12

--- 12_employee_graph/main.py
+++ 12_employee_graph/main.py
@@ -1,6 +1,12 @@
 import plotly.express as px
 import pandas as pd
 
-data_canada = px.data.gapminder().query("country == 'Canada'")
-fig = px.bar(data_canada, x='year', y='pop')
+employees_df = pd.read_csv('employees.csv')
+
+
+sorted_employee_df = employees_df.groupby(["Department"])['Salary'].mean().reset_index()
+
+
+fig = px.bar(sorted_employee_df, x="Department", y="Salary",
+             title="Average Salary by Department")
 fig.show()
